9a055e039e25f389fb5c4d71fa0ce7286993ac53
Joseph Leung Yat Chun <author2004@quickzip.org>
2013-12-11 16:51:10 +0800

    FileExplorer3: Added ISupportDragHelper and ISupportDropHelper which allow handling ISupportDrag/Drop in separate class.

---
+++ src/FileExplorer3/app/FileExplorer3/UserControls/UIEvent/ISupportDrop.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileExplorer.ViewModels.Helpers
+{
+   
+    public class QueryDropResult
+    {
+        public QueryDropResult(DragDropEffects supportedEffects, DragDropEffects preferredEffects)
+        {
+            SupportedEffects = supportedEffects;
+            PreferredEffect = preferredEffects;
+        }
+
+        public static QueryDropResult None = new QueryDropResult(DragDropEffects.None, DragDropEffects.None);            
+
+        public static QueryDropResult CreateNew(DragDropEffects supportedEffects, DragDropEffects preferredEffects)
+        {
+            return new QueryDropResult(supportedEffects, preferredEffects);
+        }
+
+         public static QueryDropResult CreateNew(DragDropEffects supportedEffects)
+        {
+            return new QueryDropResult(supportedEffects, supportedEffects);
+        }
+
+
+        public DragDropEffects SupportedEffects { get; set; }
+        public DragDropEffects PreferredEffect { get; set; }
+    }
+
+    public interface ISupportDropHelper
+    {
+        ISupportDrop DropHelper { get; }
+    }
+
+    public interface ISupportDrop
+    {
+        bool IsDroppable { get; }
+        QueryDropResult QueryDrop(IDataObject da, DragDropEffects allowedEffects);
+        IEnumerable<IDraggable> QueryDropDraggables(IDataObject da);
+        DragDropEffects Drop(IEnumerable<IDraggable> draggables, IDataObject da, DragDropEffects allowedEffects);
+    }
+}
