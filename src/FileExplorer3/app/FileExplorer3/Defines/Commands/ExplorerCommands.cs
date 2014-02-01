f6faa0d11f6c3d6ac21f8a8829e57abb0b9e1b6a
Joseph Leung Yat Chun <author2004@quickzip.org>
2014-02-01 14:25:33 +0800

    FileExplorer3: Added a refresh button in BreadcrumbView.

--- src/FileExplorer3/app/FileExplorer3/Defines/Commands/ExplorerCommands.cs
+++ src/FileExplorer3/app/FileExplorer3/Defines/Commands/ExplorerCommands.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Windows.Input;
 
-namespace QuickZip.UserControls.Input
+namespace FileExplorer.Defines
 {
     public static class ExplorerCommands
     {
-        
+        public static RoutedUICommand UpOneLevel = new RoutedUICommand(Strings.strInvertSelect, "InvertSelect", typeof(ExplorerCommands));
+        public static RoutedUICommand InvertSelect = new RoutedUICommand(Strings.strInvertSelect, "InvertSelect", typeof(ExplorerCommands));
+        public static RoutedUICommand UnselectAll = new RoutedUICommand(Strings.strUnselectAll, "UnselectAll", typeof(ExplorerCommands));
+        public static RoutedUICommand ToggleCheckBox = new RoutedUICommand(Strings.strToggleCheckBox, "ToggleCheckBox", typeof(ExplorerCommands));
+        public static RoutedUICommand ToggleViewMode = new RoutedUICommand(Strings.strToggleViewMode, "ToggleViewMode", typeof(ExplorerCommands));
+
+        public static RoutedUICommand Refresh = new RoutedUICommand(Strings.strRefresh, "Refresh", typeof(ExplorerCommands),
+            new InputGestureCollection(new InputGesture[] { new KeyGesture(Key.R, ModifierKeys.Control) }));
+
+        public static RoutedUICommand Rename = new RoutedUICommand(Strings.strRename, "Rename", typeof(ExplorerCommands),
+            new InputGestureCollection(new InputGesture[] { new KeyGesture(Key.F2) }));
     }
 }
