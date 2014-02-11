bc28b7b183215f93e573318cda8745d5b2edad29
Joseph Leung Yat Chun <author2004@quickzip.org>
2014-02-11 13:34:11 +0800

    FileExplorer3: Moved SkyDrive_Client_Id to separate class (AuthorizationKeys).

---
+++ src/FileExplorer3/app/TestApp.WPF/AuthorizationKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.WPF
+{
+    public static class AuthorizationKeys
+    {
+        public static string SkyDrive_Client_Id = "Replace your SkyDrive_Client_Id";
+        public static string GoogleDrive_Client_Id = "Replace your GoogleDrive_Client_Id";
+        public static string GoogleDrive_Client_Secret = "Replace your GoogleDrive_Client_Secret";
+    }
+}
