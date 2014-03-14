4f47632cd3c84e0c738cd229ebd2ba54e804486d
LYCJ <LYCJ@localhost>
2014-03-15 01:38:52 +0800

    FileExplorer3: Added ModelCache to DropBoxProfile. FileExplorer3: Added DropBoxModelThumbnailExtractor.

---
+++ src/FileExplorer3/app/FileExplorer3.IO/Model/DropBox/EntryModelExtractorImpl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace FileExplorer.Models
+{
+    public class DropBoxModelThumbnailExtractor : IEntryModelIconExtractor
+    {
+        public static DropBoxModelThumbnailExtractor Instance = new DropBoxModelThumbnailExtractor();
+
+        public async Task<ImageSource>
+            GetIconForModelAsync(IEntryModel model, System.Threading.CancellationToken ct)
+        {
+            var dboxModel = model as DropBoxItemModel;
+            if (dboxModel != null && dboxModel.Metadata.Thumb_Exists)
+            {
+                byte[] bytes = (await (model.Profile as DropBoxProfile).Client.GetThumbnailTask(dboxModel.Metadata, 
+                    DropNet.Models.ThumbnailSize.Large)).RawBytes;
+                
+                BitmapImage retIcon = new BitmapImage();
+
+                retIcon.BeginInit();
+                retIcon.StreamSource = new MemoryStream(bytes);
+
+                retIcon.EndInit();
+                retIcon.Freeze();
+                return retIcon;
+            }
+            return null;
+        }
+    }
+}
