a94943f01488dfddec9cdd1866e25362978cf795
LYCJ <LYCJ@localhost>
2014-05-16 17:20:15 +0800

    FileExplorer3: Fixed Issue#2 FileListParameters.ItemSize now default at 80.

--- src/FileExplorer3/app/FileExplorer3/Defines/Parameters.cs
+++ src/FileExplorer3/app/FileExplorer3/Defines/Parameters.cs
@@ -82,7 +82,7 @@ namespace FileExplorer.Defines
 
     public class FileListParameters: NotifyPropertyChanged, IFileListParameters
     {
-        private int _itemSize = 60;
+        private int _itemSize = 80;
         private string _viewMode = "Icon";
         public int ItemSize { get { return _itemSize; } set { _itemSize = value; NotifyOfPropertyChanged(() => ItemSize); } }
         public string ViewMode { get { return _viewMode; } set { _viewMode = value; NotifyOfPropertyChanged(() => ViewMode); } }
