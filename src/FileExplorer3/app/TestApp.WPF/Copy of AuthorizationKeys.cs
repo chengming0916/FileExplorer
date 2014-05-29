610d666f1e761ed02e3d7eadb766265d85e8fb75
LYCJ <LYCJ@localhost>
2014-05-30 00:59:55 +0800

    FileExplorer3: Fixed a typo in the previous Windows 7 fix. FileExplorer3: AuthofizationKeys now default as null, thus disabled. FileExplorer3: Added project for VS2013 Express and Pro edition

--- src/FileExplorer3/app/TestApp.WPF/Copy of AuthorizationKeys.cs
+++ src/FileExplorer3/app/TestApp.WPF/Copy of AuthorizationKeys.cs
@@ -10,16 +10,16 @@ namespace TestApp
     public static class AuthorizationKeys
     {
         //OneDrive API -  https://account.live.com/developers/applications
-        public static string SkyDrive_Client_Id = "Replace_your_SkyDrive_Client_Id";
-        public static string SkyDrive_Client_Secret = "Replace_your_SkyDrive_Client_Secret";
+        public static string SkyDrive_Client_Id = null; //"Replace_your_SkyDrive_Client_Id";
+        public static string SkyDrive_Client_Secret = null; //"Replace_your_SkyDrive_Client_Secret";
 
         //GoogleDrive API - https://cloud.google.com/console/project 
         //The code below is not used at this time, please download your secret to gapi_client_secret.json
-        public static string GoogleDrive_Client_Id = "Replace_your_GoogleDrive_Client_Id";
-        public static string GoogleDrive_Client_Secret = "Replace_your_GoogleDrive_Client_Secret";
+        public static string GoogleDrive_Client_Id = null; //"Replace_your_GoogleDrive_Client_Id";
+        public static string GoogleDrive_Client_Secret = null; //"Replace_your_GoogleDrive_Client_Secret";
 
         //DropBox API - https://www.dropbox.com/developers/apps/create
-        public static string DropBox_Client_Id = "Replace_your_DropBox_Client_Id";
-        public static string DropBox_Client_Secret = "Replace_your_DropBox_Client_Secret";
+        public static string DropBox_Client_Id = null; //"Replace_your_DropBox_Client_Id";
+        public static string DropBox_Client_Secret = null; //"Replace_your_DropBox_Client_Secret";
     }
 }
