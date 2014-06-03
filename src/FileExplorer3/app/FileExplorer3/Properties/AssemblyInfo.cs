962afded043681c4e06a3a59dea071958497bd06
LYCJ <LYCJ@localhost>
2014-06-03 16:22:28 +0800

    FileExplorer3: Added nant build file.

--- src/FileExplorer3/app/FileExplorer3/Properties/AssemblyInfo.cs
+++ src/FileExplorer3/app/FileExplorer3/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Resources;
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.*")]
-[assembly: AssemblyFileVersion("3.0.*")]
+// [assembly: AssemblyVersion("3.0.6.0")]
+[assembly: AssemblyVersion("3.0.6.0")]
+[assembly: AssemblyFileVersion("3.0.6.0")]
