a295eb3508cab718e79fbb42f504b9c423abc9eb
LYCJ <LYCJ@localhost>
2014-06-06 15:08:21 +0800

    FileExplorer3: Updated nant build file.

--- build/Build-FE3.bat
+++ build/Build-FE3.bat
@@ -1,2 +1,2 @@
-..\tools\nant\nant.exe init import build-revision copy -buildfile:fileExplorer3.build -logfile:compile.txt 
-pause
+..\tools\nant\nant.exe init import build-revision copy pack -buildfile:fileExplorer3.build -logfile:compile.txt 
+if not ERRORLEVEL 0 pause
