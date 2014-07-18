0f3261ffc86192196748d9179fdec8b8ebddf695
LYCJ <LYCJ@localhost>
2014-07-18 16:55:41 +0800

    FileExplorer3: FileExplorer.Scripting is now referenced to other projects, removed redundant code in FileExplorer3 library. FileExplorer3: FileExplorer3's script commands is separated into different classes, easier to read. FileExplorer3: Version update 3.0.17.0.

--- src/FileExplorer3/app/FileExplorer3/Properties/AssemblyInfo.cs
+++ src/FileExplorer3/app/FileExplorer3/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.16.3")]
-[assembly: AssemblyVersion("3.0.16.3")]
-[assembly: AssemblyFileVersion("3.0.16.3")]
+// [assembly: AssemblyVersion("3.0.17.0")]
+[assembly: AssemblyVersion("3.0.17.0")]
+[assembly: AssemblyFileVersion("3.0.17.0")]
