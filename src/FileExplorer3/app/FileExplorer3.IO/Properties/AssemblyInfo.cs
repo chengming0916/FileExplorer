dbc53d80085f57215fe4c1f207f3b900f8190b15
LYCJ <LYCJ@localhost>
2014-07-23 19:28:50 +0800

    FileExplorer3: Version update 3.0.17.5

--- src/FileExplorer3/app/FileExplorer3.IO/Properties/AssemblyInfo.cs
+++ src/FileExplorer3/app/FileExplorer3.IO/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Windows.Markup;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.17.4")]
-[assembly: AssemblyVersion("3.0.17.4")]
-[assembly: AssemblyFileVersion("3.0.17.4")]
+// [assembly: AssemblyVersion("3.0.17.5")]
+[assembly: AssemblyVersion("3.0.17.5")]
+[assembly: AssemblyFileVersion("3.0.17.5")]
