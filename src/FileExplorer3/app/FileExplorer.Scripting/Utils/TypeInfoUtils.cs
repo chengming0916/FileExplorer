9fa0677346c28a2b0f6a09844a634bf624eb68bc
LYCJ <LYCJ@localhost>
2014-08-02 02:56:02 +0800

    FileExplorer3: ParameterDic.GetValue() support "." for accessing sub-property/ method with no parameter. (e.g. {CurrentDirectory.Profile}).

--- src/FileExplorer3/app/FileExplorer.Scripting/Utils/TypeInfoUtils.cs
+++ src/FileExplorer3/app/FileExplorer.Scripting/Utils/TypeInfoUtils.cs
@@ -9,6 +9,31 @@ namespace FileExplorer.Utils
 {
     public static class TypeInfoUtils
     {
+        public static object GetPropertyOrMethod(object obj, params string[] pathSplit)
+        {
+            for (int i = 0; i < pathSplit.Length; i++)
+                obj = GetPropertyOrMethod(obj, pathSplit[i]);
+            return obj;
+        }
+
+        public static object GetPropertyOrMethod(object obj, string name)
+        {
+            if (name.EndsWith("()"))
+            {
+                var methodInfo = obj.GetType().GetTypeInfo().GetMethodInfoRecursive(name.TrimEnd('(', ')'));
+                if (methodInfo == null)
+                    throw new KeyNotFoundException(name);
+                else return methodInfo.Invoke(obj, new object[] { });
+            }
+            else
+            {
+                var propertyInfo = obj.GetType().GetTypeInfo().GetPropertyInfoRecursive(name);
+                if (propertyInfo == null)
+                    throw new KeyNotFoundException(name);
+                else return propertyInfo.GetValue(obj);
+            }
+        }
+
         public static FieldInfo GetFieldInfoRecursive(this TypeInfo typeInfo, string fieldName)
         {
             var retVal = typeInfo.DeclaredFields.FirstOrDefault(pi => pi.Name.Equals(fieldName));
