50cf77d1f7d7e9df4d02b9c26151d8de1147a2f5
LYCJ <LYCJ@localhost>
2014-11-26 18:13:45 +0800

    FileExplorer3: Nodo.

--- src/FileExplorer3/app/FileExplorer3/Models/IDragDropHandler.cs
+++ src/FileExplorer3/app/FileExplorer3/Models/IDragDropHandler.cs
@@ -10,8 +10,8 @@ namespace FileExplorer.Models
     public interface IDragDropHandler
     {        
         DragDropEffectsEx QueryDrag(IEnumerable<IEntryModel> entries);
-
         void OnDragCompleted(IEnumerable<IEntryModel> entries, DragDropEffectsEx effect);
+
         bool QueryCanDrop(IEntryModel dest);
         QueryDropEffects QueryDrop(IEnumerable<IEntryModel> entries, IEntryModel dest, DragDropEffectsEx allowedEffects);
         DragDropEffectsEx OnDropCompleted(IEnumerable<IEntryModel> entries, IEntryModel dest, DragDropEffectsEx allowedEffects);
