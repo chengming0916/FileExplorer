ba88a0c91873f232f81ef393c9a9e71cc681fa43
LYCJ <LYCJ@localhost>
2014-12-05 02:14:54 +0800

    FileExplorer3: Version update 3.0.20.3 FileExplorer3: Fixed FileExplorer (WPF) project cannot compile.

---
+++ src/FileExplorer3/app/FileExplorer3.WPF/ViewModels/AddBookmarksViewModel.cs
@@ -0,0 +1,49 @@
+﻿using FileExplorer.Models;
+using FileExplorer.Models.Bookmark;
+using FileExplorer.WPF.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExplorer3.WPF.ViewModels
+{
+    public class AddBookmarksViewModel : NotifyPropertyChanged
+    {        
+        #region fields
+
+        private IEntryModel _selectedPath;
+
+        #endregion
+
+        #region constructors
+
+        #endregion
+
+        #region events
+
+        #endregion
+
+        #region properties
+
+        public BookmarkProfile Profile { get; set; }
+
+        public IEntryModel SelectedDirectory
+        {
+            get { return _selectedPath; }
+            set
+            {
+                _selectedPath = value;
+                NotifyOfPropertyChanged(() => SelectedDirectory);                
+            }
+        }
+
+
+        #endregion
+
+        #region methods
+
+        #endregion
+    }
+}
