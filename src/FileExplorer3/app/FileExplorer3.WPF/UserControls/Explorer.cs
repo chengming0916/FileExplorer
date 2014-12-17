83d22d0fbeb4a3ea1e81fa98419d33fac4b3ef3a
LYCJ <LYCJ@localhost>
2014-12-17 13:25:13 +0800

    FileExplorer3: Added EnableContextMenu/Drag/Drop/MultiSelect/Bookmark dependency property to Explorer usercontrol.

--- src/FileExplorer3/app/FileExplorer3.WPF/UserControls/Explorer.cs
+++ src/FileExplorer3/app/FileExplorer3.WPF/UserControls/Explorer.cs
@@ -33,10 +33,10 @@ namespace FileExplorer.WPF.UserControls
         {
             _wm = new WindowManager();
             _events = new EventAggregator();
-            _evm = new ExplorerViewModel(_wm, _events);            
+            _evm = new ExplorerViewModel(_wm, _events);
 
-            this.SetBinding(CurrentDirectoryProperty, new Binding("FileList.CurrentDirectory") { Source = _evm, Mode= BindingMode.TwoWay });
-            this.SetBinding(SelectedEntriesProperty, new Binding("FileList.Selection.SelectedModels") { Source = _evm, Mode= BindingMode.TwoWay });            
+            this.SetBinding(CurrentDirectoryProperty, new Binding("FileList.CurrentDirectory") { Source = _evm, Mode = BindingMode.TwoWay });
+            this.SetBinding(SelectedEntriesProperty, new Binding("FileList.Selection.SelectedModels") { Source = _evm, Mode = BindingMode.TwoWay });
         }
 
         #endregion
@@ -47,28 +47,56 @@ namespace FileExplorer.WPF.UserControls
         {
             Explorer exp = sender as Explorer;
 
-            if (args.Property == RootDirectoriesProperty)
-            {
-                IEntryModel[] newRootDirs = args.NewValue as IEntryModel[];
-                if (newRootDirs != null && newRootDirs.Length > 0)
-                    exp._evm.Commands.Execute(                       
-                        UIScriptCommands.NotifyRootChanged(newRootDirs)
-                        );
-            }
-            else if (args.Property == ModeProperty)
-            {
-                switch ((ExplorerMode)args.NewValue)
-                {
-                    case ExplorerMode.Normal: exp.Content = new ExplorerView(); break;
-                    case ExplorerMode.ToolWindow: exp.Content = new ToolWindow(); break;
-                    case ExplorerMode.ToolWindow2: exp.Content = new ToolWindow2(); break;
-                    case ExplorerMode.DirectoryPicker: exp.Content = new DirectoryPicker(); break;
-                    default: exp.Content = null; break;
-                }
 
-                if (exp.Content != null)
-                    Caliburn.Micro.Bind.SetModel(exp.Content as DependencyObject, exp._evm);
+            switch (args.Property.Name)
+            {
+                case "RootDirectories":
+                    IEntryModel[] newRootDirs = args.NewValue as IEntryModel[];
+                    if (newRootDirs != null && newRootDirs.Length > 0)
+                        exp._evm.Commands.Execute(
+                            UIScriptCommands.NotifyRootChanged(newRootDirs)
+                            );
+                    break;
+                case "Mode":
+                    switch ((ExplorerMode)args.NewValue)
+                    {
+                        case ExplorerMode.Normal: exp.Content = new ExplorerView(); break;
+                        case ExplorerMode.ToolWindow: exp.Content = new ToolWindow(); break;
+                        case ExplorerMode.ToolWindow2: exp.Content = new ToolWindow2(); break;
+                        case ExplorerMode.DirectoryPicker: exp.Content = new DirectoryPicker(); break;
+                        default: exp.Content = null; break;
+                    }
+
+                    if (exp.Content != null)
+                        Caliburn.Micro.Bind.SetModel(exp.Content as DependencyObject, exp._evm);
+                    break;
+                case "EnableContextMenu":
+                case "EnableDrag":
+                case "EnableDrop":
+                case "EnableBookmark":
+                case "EnableMultiSelect":
+                    ExplorerParameterType prop;
+                    if (Enum.TryParse<ExplorerParameterType>(args.Property.Name, true, out prop))
+                        exp.setVMProperty(prop, (bool)args.NewValue);
+                    break;
+
             }
+
+
+        }
+
+        private T getVMProperty<T>(ExplorerParameterType property)
+        {
+            ParameterDic pd = new ParameterDic();
+            _evm.Commands.Execute(UIScriptCommands.ExplorerGetParameter(property, "{OutputValue}"), pd);
+            return pd.GetValue<T>("{OutputValue}");
+        }
+
+        private void setVMProperty<T>(ExplorerParameterType property, T value)
+        {
+            ParameterDic pd = new ParameterDic();
+            pd.SetValue("{PropertyValue}", value, false);
+            _evm.Commands.Execute(UIScriptCommands.ExplorerSetParameter(property, "{PropertyValue}"), pd);
         }
 
         #endregion
@@ -87,6 +115,51 @@ namespace FileExplorer.WPF.UserControls
 
         public IWindowManager WindowManager { get { return _wm; } set { _wm = value; } }
         public IEventAggregator Events { get { return _events; } set { _events = value; } }
+
+        public static DependencyProperty EnableContextMenuProperty = DependencyProperty.Register("EnableContextMenu",
+             typeof(bool), typeof(Explorer), new PropertyMetadata(true, OnPropertiesChanged));
+
+        public bool EnableContextMenu
+        {
+            get { return (bool)GetValue(EnableContextMenuProperty); }
+            set { SetValue(EnableContextMenuProperty, value); }
+        }
+
+        public static DependencyProperty EnableDragProperty = DependencyProperty.Register("EnableDrag",
+            typeof(bool), typeof(Explorer), new PropertyMetadata(true, OnPropertiesChanged));
+
+        public bool EnableDrag
+        {
+            get { return (bool)GetValue(EnableDragProperty); }
+            set { SetValue(EnableDragProperty, value); }
+        }
+
+        public static DependencyProperty EnableDropProperty = DependencyProperty.Register("EnableDrop",
+            typeof(bool), typeof(Explorer), new PropertyMetadata(true, OnPropertiesChanged));
+
+        public bool EnableDrop
+        {
+            get { return (bool)GetValue(EnableDropProperty); }
+            set { SetValue(EnableDropProperty, value); }
+        }
+
+        public static DependencyProperty EnableMultiSelectProperty = DependencyProperty.Register("EnableMultiSelect",
+            typeof(bool), typeof(Explorer), new PropertyMetadata(true, OnPropertiesChanged));
+
+        public bool EnableMultiSelect
+        {
+            get { return (bool)GetValue(EnableMultiSelectProperty); }
+            set { SetValue(EnableMultiSelectProperty, value); }
+        }
+
+        public static DependencyProperty EnableBookmarkProperty = DependencyProperty.Register("EnableBookmark",
+            typeof(bool), typeof(Explorer), new PropertyMetadata(false, OnPropertiesChanged));
+
+        public bool EnableBookmark
+        {
+            get { return (bool)GetValue(EnableBookmarkProperty); }
+            set { SetValue(EnableBookmarkProperty, value); }
+        }
 
         public static DependencyProperty ModeProperty = DependencyProperty.Register("Mode",
             typeof(ExplorerMode), typeof(Explorer), new PropertyMetadata(ExplorerMode.Unknown, OnPropertiesChanged));
@@ -119,7 +192,7 @@ namespace FileExplorer.WPF.UserControls
 
         public static DependencyProperty RootDirectoriesProperty = DependencyProperty.Register("RootDirectories",
             typeof(IEntryModel[]), typeof(Explorer), new PropertyMetadata(null, OnPropertiesChanged));
-     
+
         public IEntryModel[] RootDirectories
         {
             get { return (IEntryModel[])GetValue(RootDirectoriesProperty); }
@@ -127,7 +200,7 @@ namespace FileExplorer.WPF.UserControls
         }
 
 
-     
+
         #endregion
 
     }
