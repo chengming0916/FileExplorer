ecda08b5be96fb6fda4823da50ecce64405c9db8
LYCJ <LYCJ@localhost>
2015-01-06 15:18:32 +0800

    FileExplorer3: Added Test_IOCPanelTest, not completed.

---
+++ src/FileExplorer3/app/Test_IOCPanelTest/Test_IOCPanelTest/ValueViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_IOCPanelTest
+{
+    public class ValueViewModel : NotifyPropertyChanged
+    {
+        public int Value { get; set; }
+        public double Height { get; set; }
+
+        public ValueViewModel(int value)
+        {
+            Value = value;
+            Height = (new Random().NextDouble() * 15) + 19;
+        }
+
+    }
+}
